e64806434cca6e450ffb9597c936cca81acd69d9
Martin <martinreberg1@live.no>
2023-04-27 14:48:48 +0200

    first

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reberg Programmering</title>
+    <link rel="stylesheet" href="style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="top">
+                <img style="height: 300px;" src="icon/RP.png"/>
+                <h1>Reberg Programmering</h1>
+            </div>
+            <div class="bottom">
+
+            </div>
+        </div>
+        <div class="separator"></div>
+        <div class="content">
+
+        </div>
+    </div>
+</body>
+</html>
